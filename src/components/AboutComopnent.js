8dad8b08afe3e146b5c5cae366ddc322987b1ff0
psbharadwaj2002 <psbharadwaj.scsvmv@gmail.com>
2023-12-31 01:16:36 +0530

    about-component-responsive

--- src/components/AboutComopnent.js
+++ src/components/AboutComopnent.js
@@ -1,10 +1,11 @@
 import React from "react";
 import my_image from "../assests/my_image.png";
+import { Link } from "react-router-dom";
 
 function AboutComopnent() {
   return (
     <div className="aboutComponent">
-      <div>
+      <div className="container">
         <img src={my_image} alt="my_image" />
         <div className="about_skill">
           <div className="about">
@@ -17,15 +18,44 @@ function AboutComopnent() {
                 MERN stack developer with a passion for web development. Eager
                 to learn, apply cutting-edge technologies, and contribute fresh
                 perspectives to create innovative solutions. Bringing a strong
-                work ethic to every project. Let's embark on this coding journey
-                together!
+                work ethic to every project.
               </p>
             </div>
+            <Link
+              to="aboutMe"
+              style={{ textDecoration: "none", color: "grey", fontSize: 12 }}
+            >
+              To know more about me Click Here
+            </Link>
           </div>
           <div className="skill">
             <div className="title">
               SKILLS
               <div></div>
+            </div>
+            <div className="skills">
+              <ul>
+                <li>MERN Stack</li>
+                <li>Redux</li>
+                <li>Python</li>
+                <li>MySQL</li>
+                <li>OOPS</li>
+              </ul>
+              <ul>
+                <li>REST API's</li>
+                <li>System Design</li>
+                <li>Linux</li>
+                <li>Git</li>
+                <li>GitHub</li>
+              </ul>
+            </div>
+            <div>
+              <Link
+                to="projects"
+                style={{ textDecoration: "none", color: "grey", fontSize: 12 }}
+              >
+                To know more about my skills Click Here
+              </Link>
             </div>
           </div>
         </div>
