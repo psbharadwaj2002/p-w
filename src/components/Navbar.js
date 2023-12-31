7f41f97394c1ff275c601d7d8912148359a44536
psbharadwaj2002 <psbharadwaj.scsvmv@gmail.com>
2023-12-31 23:59:09 +0530

    about me page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,51 +4,49 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <div>
-      <nav className="navbar navbar-expand-lg ">
-        <div className="container-fluid">
-          <Link className="navbar-brand" to="/">
-            SRINIVASA BHARADWAJ PEDDINTI
-          </Link>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNavDropdown"
-            aria-controls="navbarNavDropdown"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-            style={{ border: "1px solid white", color: "white" }}
-          >
-            <MenuOutlined />
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNavDropdown">
-            <ul className="navbar-nav ms-auto">
-              <li className="nav-item">
-                <Link className="nav-link" aria-current="page" to="/">
-                  HOME
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/aboutMe">
-                  ABOUT ME
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/projects">
-                  PROJECTS
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/contact">
-                  CONTACT
-                </Link>
-              </li>
-            </ul>
-          </div>
+    <nav className="navbar navbar-expand-lg">
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/">
+          BHARADWAJ
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavDropdown"
+          aria-controls="navbarNavDropdown"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+          style={{ border: "1px solid white", color: "white" }}
+        >
+          <MenuOutlined />
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <Link className="nav-link" aria-current="page" to="/">
+                HOME
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/aboutMe">
+                ABOUT ME
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/projects">
+                PROJECTS
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/contact">
+                CONTACT
+              </Link>
+            </li>
+          </ul>
         </div>
-      </nav>
-    </div>
+      </div>
+    </nav>
   );
 }
 
